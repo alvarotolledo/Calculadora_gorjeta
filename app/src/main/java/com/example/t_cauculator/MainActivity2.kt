4dd7efc6db902448d2307b72becc676e120490f9
Alvaro Toledo <125941213+alvarotolledo@users.noreply.github.com>
2024-06-18 22:38:35 +0100

    Passando Pacotes Para a proxima tela, recuperei os valorews

--- app/src/main/java/com/example/t_cauculator/MainActivity2.kt
+++ app/src/main/java/com/example/t_cauculator/MainActivity2.kt
@@ -5,12 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.t_cauculator.databinding.ActivityMain2Binding
+
+private lateinit var binding: ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val total_pessoas = intent.getIntExtra("total_persona", 0)
+        val valor_conta = intent.getFloatExtra("valor_conta", 0f)
+        val Porcentagem = intent.getIntExtra("porcentage",0)
+        val valor_total = intent.getFloatExtra("valor_total", 0F)
 
 
 
